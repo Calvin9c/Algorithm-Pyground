3c62e33a28b57ff211b18900888b44b3af8f3801
Calvin9c <m.acc.ch1230@gmail.com>
2025-05-23 13:55:41 +0800

    refatcor sortlib

--- sortlib/cpp/include/sorting.h
+++ sortlib/cpp/include/sorting.h
@@ -1,6 +1,4 @@
-#ifndef SORTING_H
-#define SORTING_H
-
+#pragma once
 #include <vector>
 
 template <typename T>
@@ -14,21 +12,10 @@ void selection_sort(std::vector<T>& data);
 
 /*merge sort*/
 template <typename T>
-std::vector<T> _merge(const std::vector<T>& l, const std::vector<T>& r);
-template <typename T>
 std::vector<T> merge_sort(const std::vector<T>& data);
 
 /*quick sort*/
 template <typename T>
-size_t partition(std::vector<T>& data, size_t low, size_t high);
-template <typename T>
-void _quick_sort_helper_impl_0(std::vector<T>& data, size_t low, size_t high);
-template<typename T>
-void _quick_sort_helper_impl_1(std::vector<T>& data, size_t l, size_t r);
-
-template <typename T>
 void quick_sort(std::vector<T>& data /*, int impl=0*/);
 
 #include "sorting.tpp"
-
-#endif
